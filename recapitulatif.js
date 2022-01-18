9433c6a0cf829ed7fc1119e129b84d15dadbc419
AlexisRomney <alexis.romney@hotmail.fr>
2022-01-18 12:54:55 +0100

    alignement des étoiles

--- recapitulatif.js
+++ recapitulatif.js
@@ -2,10 +2,10 @@
     let togg1 = document.getElementById("togg1");
     let d1 = document.getElementById("d1");
     
-      if(getComputedStyle(d1).display != "block"){
-        d1.style.display = "none";
+      if(getComputedStyle(d1).visibility != "hidden"){
+        d1.style.visibility = "hidden";
       } else {
-        d1.style.display = "block";
+        d1.style.visibility = "visible";
       }
     
     togg1.onclick = togg ;
