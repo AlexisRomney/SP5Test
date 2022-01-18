4eb1023e878e9c12beb3dc153bf10bd81722c5c7
AlexisRomney <alexis.romney@hotmail.fr>
2022-01-18 11:42:56 +0100

    test

--- recapitulatif.js
+++ recapitulatif.js
@@ -2,7 +2,7 @@
     let togg1 = document.getElementById("togg1");
     let d1 = document.getElementById("d1");
     
-      if(getComputedStyle(d1).display != "none"){
+      if(getComputedStyle(d1).display != "block"){
         d1.style.display = "none";
       } else {
         d1.style.display = "block";
