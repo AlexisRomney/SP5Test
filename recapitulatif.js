9a4c2ed86907173b8365eaede5ed17997cb4ce43
AlexisRomney <alexis.romney@hotmail.fr>
2022-01-18 10:29:38 +0100

    debut css

--- recapitulatif.js
+++ recapitulatif.js
@@ -1,11 +1,13 @@
-let togg1 = document.getElementById("togg1");
-let d1 = document.getElementById("d1");
-function togg(){
-    if(getComputedStyle(d1).display != "none"){
-      d1.style.display = "none";
-    } else {
-      d1.style.display = "block";
-    }
-  };
-  togg1.onclick = togg & console.log("coucou");
+  window.onload = function togg(){ 
+    let togg1 = document.getElementById("togg1");
+    let d1 = document.getElementById("d1");
+    
+      if(getComputedStyle(d1).display != "none"){
+        d1.style.display = "none";
+      } else {
+        d1.style.display = "block";
+      }
+    
+    togg1.onclick = togg ;
+}; 
 
